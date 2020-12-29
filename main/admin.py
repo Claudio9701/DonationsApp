20d59436e9b21b4c0956dc0a6a77a99ffac225ec
Claudio <claudio.rtega2701@gmail.com>
2020-12-28 20:05:13 -0500

    minor update

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(Beneficiary)
+admin.site.register(Donor)
+admin.site.register(Partner)
+admin.site.register(Product)
+admin.site.register(Donation)
